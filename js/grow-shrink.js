ccf6dfe26102f7aede18fcb319f6f0112875a1ca
jchristofferson <blackchakram@gmail.com>
2018-02-12 12:35:58 -0700

    back button attempt 3

--- js/grow-shrink.js
+++ js/grow-shrink.js
@@ -111,7 +111,7 @@ $(document).keyup(function(e) {
 
 
 // intercept a back button press and redirect to main site again
-window.onbeforeunload = function (e) {
+window.onpopstate = function (e) {
       console.log("re");
   if ($exp_project == true && $exp_image == false && $inprogress == false && $contactopen == false) {
     $inprogress = true;
